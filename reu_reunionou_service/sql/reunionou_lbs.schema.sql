3f9364cd064ffece7fc794cb05ebb85172123e7b
akrem jomaa <jomaakrem@gmail.com>
2023-04-05 09:41:51 +0200

    adding invitation functionalities

---
+++ reu_reunionou_service/sql/reunionou_lbs.schema.sql
@@ -0,0 +1,66 @@
+-- phpmyadmin 4.8.1 MySQL 5.5.5-10.10.2-MariaDB-1:10.10.2+maria~ubu2204 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+USE `reunionou_lbs`;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT,
+    `name`      varchar(128) NOT NULL,
+    `firstname` varchar(128) NOT NULL,
+    `email`     varchar(255) NOT NULL,
+    `password`  varchar(128) NOT NULL,
+    `status` varchar(255) DEFAULT NULL,
+    `refresh_token`    varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE `event`
+(
+    `id`          int(11) NOT NULL AUTO_INCREMENT,
+    `title`       varchar(255) NOT NULL,
+    `description` text DEFAULT NULL,
+    `lieu`        varchar(255) NOT NULL,
+    `date`        datetime     NOT NULL,
+    `status` varchar(255) NOT NULL,
+    `user_id`     int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY           `user_id` (`user_id`),
+    CONSTRAINT `event_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `invitation`;
+CREATE TABLE `invitation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `invitation_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `user_id` int(11) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_invitation` (`user_id`,`event_id`),
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `invitation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `invitation_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `event_id` (`event_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 2023-04-03 07:34:05
