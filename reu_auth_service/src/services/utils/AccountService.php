ff1b4a5cddf5c7a5b9a014c86c37280ec06f3a95
KhaoulaCode <boulhdir.khaoula@gmail.com>
2023-04-05 11:44:45 +0200

    update

---
+++ reu_auth_service/src/services/utils/AccountService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace auth\services\utils;
+use auth\models\User;
+
+
+final class AccountService {
+
+    public static function getUserByUsername(string $username): ?User {
+        return User::select('id', 'name', 'firstname', 'email', 'password', 'status', 'refresh_token')
+                ->where('email', '=', $username)
+                ->firstOrFail();
+
+    }
+    
+    public static function getUserById(string $id): ?User {
+        return User::select('id', 'name', 'firstname', 'email', 'password', 'status', 'refresh_token')
+                ->where('id', '=', $id)
+                ->firstOrFail();
+    }
+    
+    public static function updateRefreshToken(int $id, string $refreshToken): void {
+        User::where('id', '=', $id)
+        ->update(['refresh_token' => $refreshToken]);
+    }
+    
+
+}
