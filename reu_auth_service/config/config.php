94860194b9bfac04e20d3e7dc83c56f13117092f
WZidane <wail.zidane@hotmail.com>
2023-04-06 14:40:54 +0200

    update

--- reu_auth_service/config/config.php
+++ reu_auth_service/config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'username' => 'reunionou_lbs',
-    'password' => 'authR00t',
+    'password' => 'reunionou_lbs',
     'host' => 'reunionou.db',
     'secretJWT' => 'E7EF06160A495F1EC96520C385E7D449BD01509C410F8E7599C8D1E9F8C1ED1F',
 ];
--- reu_auth_service/src/models/User.php
+++ reu_auth_service/src/models/User.php
@@ -10,19 +10,7 @@ class User extends Model {
     protected $table = 'user';
     protected  $primaryKey = 'id';
     public $timestamps = false;
+    public $incrementing = false;
+    public $keyType = 'string';
 
-
-    public function events() : HasMany
-    {
-        return $this->hasMany(Event::class, 'id', 'id');
-    }
-    public function comments() : HasMany
-    {
-        return $this->hasMany(Comment::class, 'id', 'id');
-    }
-
-    public function invitations() : HasMany
-    {
-        return $this->hasMany(Invitation::class, 'id', 'id');
-    }
 }
